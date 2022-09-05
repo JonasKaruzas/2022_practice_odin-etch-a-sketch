18206bb46d9244e575b6dd0b016f734e38e5ee6c
JonasKaruzas <jonas.karuzas@gmail.com>
2022-09-05 21:40:12 +0300

    feat: :sparkles: mousemove event + click "draw's"

--- script/script.js
+++ script/script.js
@@ -3,5 +3,34 @@ const gameSize = document.querySelector("#size");
 
 gameSize.addEventListener("input", () => {
   const sizeValue = document.querySelector("#sizeValue");
-  sizeValue.innerText = gameSize.value;
+  sizeValue.innerText = `${gameSize.value} x ${gameSize.value}`;
+  gameBoard.innerHTML = null;
+  createBoard(gameSize.value, drawBox);
 });
+
+const drawBox = (e) => {
+  if (e.buttons == 1) {
+    e.target.style.backgroundColor = "red";
+  }
+};
+
+const createBoard = (size, clickHandler) => {
+  const container = document.createElement("div");
+  container.classList.add("container");
+
+  for (let i = 0; i < size; i++) {
+    const row = document.createElement("div");
+    row.classList.add("row");
+    for (let j = 0; j < size; j++) {
+      const cell = document.createElement("div");
+      cell.classList.add("cell");
+      cell.addEventListener("mousemove", clickHandler);
+      row.append(cell);
+    }
+    container.append(row);
+  }
+
+  gameBoard.append(container);
+};
+
+createBoard(5, drawBox);
